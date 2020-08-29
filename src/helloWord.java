f318fbb853b8dd46bf6e86095f1b29c65b6c3d28
chuckleberrynip <61253901+chuckleberrynip@users.noreply.github.com>
2020-08-29 11:35:00 -0400

    Add files via upload

---
+++ src/helloWord.java
@@ -0,0 +1,8 @@
+//Mohammed Azad
+import java.util.Scanner;
+import java.io.*;
+public class helloWord { 
+	public static void main(String[] args) throws Exception { 
+		System.out.println("Hello World");
+	}
+}
